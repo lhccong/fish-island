e8fdeed946e3d86f4c64a173b17312309065ab83
lhccong <771901874@qq.com>
2025-04-02 21:02:11 +0800

    fix：修改静态文件相对路径

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "//fish-island/",
+  base: "/fish-island/",
 
   lang: "zh-CN",
   title: "文档演示",
